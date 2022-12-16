aa31b625a60a45996ad228580d85cdad96504b86
adindozo <adin.dozo1@outlook.com>
2022-12-16 08:48:35 +0100

    testing commit, changed git user credentials

--- routes/mymeetings.js
+++ routes/mymeetings.js
@@ -17,22 +17,41 @@ let dbconfig = { //database credentials stored in object
 const pool = new pg.Pool(dbconfig); //creating db pool
 const router = express.Router();
 
+const multer = require('multer');
+const fs = require('fs').promises;
+
+const upload = multer({ dest: 'uploads/' });
 
 router.get('/', (req, res) => {
-    res.render('mymeetings',{username: req.user.username});
+    res.render('mymeetings', { username: req.user.username });
 })
 
 //API for retrieving list of meetings in JSON
 
 router.get('/meeting_list_json', async (req, res) => {
-    let meetings = await pool.query('select * from meetings where accountID=$1',[req.user.accountid]);
+    let meetings = await pool.query('select * from meetings where accountID=$1', [req.user.accountid]);
     res.json(meetings.rows);
 })
 
-//API for adding meeting
+// Crud API for adding meeting
 
-router.post('/add_meeting', async (req, res) => {
-    
+router.post('/add_meeting' ,upload.single('file'), async (req, res) => {
+    try {
+        if ((await pool.query('select code from meetings where code = $1',[req.body.code])).rowCount!=0) return res.status(409).json({error: 'Code already used'});
+        await pool.query(`insert into meetings 
+            (name,code,accountid,unixstart,unixend,coverphoto) values
+            ($1,$2,$3,$4,$5,$6)
+            `, [req.body.name, req.body.code, req.user.accountid, req.body.unixstart, req.body.unixend, Boolean(req.body.coverphoto)]);
+        if(!req.body.coverphoto) return res.sendStatus(200); //else store that photo in server with meetingID filename
+        let picture = await fs.readFile(req.body.pic);
+        await fs.writeFile('./../meeting_pictures/'+2+'.jpg',picture);
+        res.sendStatus(200);
+        //add default pic
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500);
+    }
+
 })
 
 
@@ -42,4 +61,4 @@ router.post('/add_meeting', async (req, res) => {
 
 
 
-module.exports=router;
+module.exports = router;
