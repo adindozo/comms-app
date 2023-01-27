48163b60cf4c9d49c63a2a9b795758f0d1f9b0d1
adindozo <adin.dozo1@outlook.com>
2023-01-27 10:35:41 +0100

    final

--- public/js/admin-client.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('click',(e)=>{
-    document.querySelector('.menu').style.left='0px';
-})
-
-document.querySelector('html').addEventListener('click',(e)=>{
-    if(e.target.id!='menu-toggle') document.querySelector('.menu').style.left='-250px';
-})
